cbd83511206e625e31de84407ca7d1235a7aa6b9
Will <wberryman3@huskers.unl.edu>
2026-08-27 17:13:59 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Will Berryman
+ *         wberryman3@huskers.unl.edu
+ * Date: 2026/08/26
+ *
+ * A me program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Will\n");
+
+  return 0;
+}
